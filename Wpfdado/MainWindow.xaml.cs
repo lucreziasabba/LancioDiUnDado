7c7234a6979fa5ea3b43d2df66c4ced4685f026a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-12-02 09:28:41 +0100

    Add project files.

---
+++ Wpfdado/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpfdado
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Estrazione_Click(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(txtNum.Text);
+            Random random_generator = new Random();
+            int RandomNumber = random_generator.Next(1,7);
+            if (n == RandomNumber)
+                txtCasuale.Text = ($"Il numero uscito è {RandomNumber}. Complimenti!Hai vinto!");
+            else if (n != RandomNumber)
+                txtCasuale.Text = ($"Il numero uscito è {RandomNumber}. Ritenta!");
+        }
+
+        private void Riprova_Click(object sender, RoutedEventArgs e)
+        {
+             txtNum.Clear();
+            txtCasuale.Clear();
+        }
+    }
+}
